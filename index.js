72bc290a98a54159e82de222fbd622fcb95ec920
Zubair Ahmad <115360512+Zu33air@users.noreply.github.com>
2023-01-16 07:26:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Computer Generated number
+const compInput = Math.floor(Math.random() * 10);
+console.log(`You have only three attempts`);
+async function userGuess() {
+    let userInput = await inquirer.prompt([
+        {
+            name: "userGuess",
+            type: "number",
+            message: "Enter any number from 1 to 10",
+        },
+    ]);
+    if (userInput.userGuess === compInput) {
+        console.log(`You Win! Correct Guess`);
+    }
+    if (userInput.userGuess > compInput) {
+        console.log(`You Lost! Your Guess is too High`);
+    }
+    else if (userInput.userGuess < compInput) {
+        console.log(`Sorry Lost! Your guess is too Low`);
+    }
+}
+await userGuess();
+doAgain();
+let userChoice = "Y";
+async function doAgain() {
+    let doAgain = await inquirer.prompt([
+        {
+            name: "tryAgain",
+            type: "input",
+            message: "Do you want to Try again Y or N",
+        },
+    ]);
+    if (doAgain.tryAgain == userChoice) {
+        userGuess();
+    }
+    else {
+        console.log("Nice Try");
+    }
+}
